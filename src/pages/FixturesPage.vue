997cbf9e0c41020bf71267e4ab0a969fb937c8b6
Hamit Uyar <60771180+uyarhamit@users.noreply.github.com>
2025-12-29 09:39:58 +0300

    Add conditional rendering for simulation button in FixturesPage

--- src/pages/FixturesPage.vue
+++ src/pages/FixturesPage.vue
@@ -23,7 +23,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex justify-center py-2">
+  <div class="flex justify-center py-2" v-if="fixtureStore.fixtures.length">
     <Button :loading="false" :text="'Simulate All'" @click="handleAllFixtureSimulation()" />
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-5 gap-5 m-5">
